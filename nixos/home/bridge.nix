c85f4ce8b5fe78eb2a91523ee28c676581765449
doyouwantto2 <tinhphong2580@gmail.com>
2025-10-23 00:16:16 +0900

    remove mkoutofsymlink

--- nixos/home/bridge.nix
+++ nixos/home/bridge.nix
@@ -2,7 +2,7 @@
 
 {
   home.file = {
-    "cava".source = config.lib.file.mkOutOfStoreSymlink ./extra/dotfiles/cava;
-    "hypr".source = config.lib.file.mkOutOfStoreSymlink ./extra/dotfiles/hypr;
+    "cava".source = ./extra/dotfiles/cava;
+    "hypr".source = ./extra/dotfiles/hypr;
   };
 }
