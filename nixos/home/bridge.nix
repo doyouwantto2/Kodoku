dd5bb816b0da1a02385b570a29f35b0502cb7da0
doyouwantto2 <tinhphong2580@gmail.com>
2025-10-23 00:10:01 +0900

    remove home.file

--- nixos/home/bridge.nix
+++ nixos/home/bridge.nix
@@ -1,8 +1,6 @@
-{ config, pkgs, lib, ... }:
+{ config, pkgs, lib, xdg, ... }:
 
 {
-  home.file = {
-    xdg.configFile."cava".source = config.lib.file.mkOutOfStoreSymlink "./extra/dotfiles/cava";
-    xdg.configFile."hypr".source = config.lib.file.mkOutOfStoreSymlink "./extra/dotfiles/hypr";
-  };
+  xdg.configFile."cava".source = config.lib.file.mkOutOfStoreSymlink "./extra/dotfiles/cava";
+  xdg.configFile."hypr".source = config.lib.file.mkOutOfStoreSymlink "./extra/dotfiles/hypr";
 }
