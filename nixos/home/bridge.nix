3f1767b1deb07b8a702062548ce979c10b852fb5
doyouwantto2 <tinhphong2580@gmail.com>
2025-10-22 23:57:26 +0900

    move hypr to home directory

--- nixos/home/bridge.nix
+++ nixos/home/bridge.nix
@@ -3,5 +3,6 @@
 {
   home.file = {
     ".config/cava".source = config.lib.file.mkOutOfStoreSymlink ./extra/dotfiles/cava;
+    ".config/hypr".source = config.lib.file.mkOutOfStoreSymlink ./extra/dotfiles/hypr;
   };
 }
