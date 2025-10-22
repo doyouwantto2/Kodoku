06c945e1e4c58cf51f0a2a06c1e993b737b4d37c
doyouwantto2 <tinhphong2580@gmail.com>
2025-10-22 22:58:24 +0900

    try out oh-my-zsh from home

--- nixos/home/userConfig.nix
+++ nixos/home/userConfig.nix
@@ -18,6 +18,16 @@
       enableCompletion = true;
       autosuggestion.enable = true;
       syntaxHighlighting.enable = true;
+
+      oh-my-zsh = {
+        enable = true;
+        plugins = [
+          "git"
+          "sudo"
+          "z"
+        ];
+      };
+
     };
 
     git = {
