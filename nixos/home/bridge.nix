eff09c553a0dbf5bdcd5b1821b4ac4189884239e
doyouwantto2 <tinhphong2580@gmail.com>
2025-10-23 00:05:31 +0900

    convert relative path to string path

--- nixos/home/bridge.nix
+++ nixos/home/bridge.nix
@@ -2,7 +2,7 @@
 
 {
   home.file = {
-    ".config/cava".source = config.lib.file.mkOutOfStoreSymlink ./extra/dotfiles/cava;
-    ".config/hypr".source = config.lib.file.mkOutOfStoreSymlink ./extra/dotfiles/hypr;
+    ".config/cava".source = config.lib.file.mkOutOfStoreSymlink "./extra/dotfiles/cava";
+    ".config/hypr".source = config.lib.file.mkOutOfStoreSymlink "./extra/dotfiles/hypr";
   };
 }
