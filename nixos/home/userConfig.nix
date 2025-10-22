0e8a67f8aee4ef9895928b1a7c1bac0cb717b268
doyouwantto2 <tinhphong2580@gmail.com>
2025-10-22 22:53:37 +0900

    fix syntax

--- nixos/home/userConfig.nix
+++ nixos/home/userConfig.nix
@@ -16,7 +16,7 @@
     zsh = {
       enable = true;
       enableCompletion = true;
-      autosuggestions.enable = true;
+      autosuggestion.enable = true;
       syntaxHighlighting.enable = true;
     };
 
