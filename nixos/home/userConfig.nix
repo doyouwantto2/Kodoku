ffeb97c03caa349e667bd9f1d1756088a200326a
doyouwantto2 <tinhphong2580@gmail.com>
2025-10-22 22:10:26 +0900

    add plugins for fish

--- nixos/home/userConfig.nix
+++ nixos/home/userConfig.nix
@@ -4,6 +4,14 @@
   programs = {
     fish = {
       enable = true;
+
+      interactiveShellInit = ''
+        set fish_greeting 
+      '';
+
+      plugins = [
+        { name = "grc"; src = pkgs.fishPlugins.grc.src; }
+      ];
     };
 
     git = {
