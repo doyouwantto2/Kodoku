aea32b5e895330e0ccd8c6205e51d6370e760332
doyouwantto2 <tinhphong2580@gmail.com>
2025-10-24 02:23:02 +0900

    update

--- nixos/system/startup/essence.nix
+++ nixos/system/startup/essence.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 {
   programs.hyprland = {
@@ -8,19 +8,17 @@
   programs.nix-ld.enable = true;
   programs.xwayland.enable = true;
 
-  environment.systemPackages = with pkgs; [
-    git
-    wezterm
-    kitty
-    wl-clipboard
-    xdg-desktop-portal
-    xdg-desktop-portal-hyprland
-    openssl
-    pkg-config
-    glib
-    gobject-introspection
-    gtk3
-    gtk4
-    libiconv
-  ];
+  environment.sessionVariables = {
+    OPENSSL_DIR = "${pkgs.openssl.dev}";
+    OPENSSL_LIB_DIR = "${pkgs.openssl.out}/lib";
+    OPENSSL_INCLUDE_DIR = "${pkgs.openssl.dev}/include";
+
+    PKG_CONFIG_PATH = lib.makeSearchPath "lib/pkgconfig" [
+      pkgs.openssl.dev
+      pkgs.glib.dev
+      pkgs.gobject-introspection.dev
+      pkgs.gtk3.dev
+      pkgs.gtk4.dev
+    ];
+  };
 }
