8de592f35ee6e86ff57107c98d3e0369ab1d404e
doyouwantto2 <tinhphong2580@gmail.com>
2025-10-24 02:18:31 +0900

    update

--- nixos/system/startup/native.nix
+++ nixos/system/startup/native.nix
@@ -12,5 +12,13 @@
     OPENSSL_DIR = "${pkgs.openssl.dev}";
     OPENSSL_LIB_DIR = "${pkgs.openssl.out}/lib";
     OPENSSL_INCLUDE_DIR = "${pkgs.openssl.dev}/include";
+
+    PKG_CONFIG_PATH = lib.makeSearchPath "lib/pkgconfig" [
+      pkgs.openssl.dev
+      pkgs.glib
+      pkgs.gtk3
+      pkgs.gtk4
+    ];
   };
+
 }
