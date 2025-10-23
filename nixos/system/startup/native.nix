90cc0ee29f7900953ec81d78584506402716b54d
doyouwantto2 <tinhphong2580@gmail.com>
2025-10-24 02:06:25 +0900

    update

--- nixos/system/startup/native.nix
+++ nixos/system/startup/native.nix
@@ -1,17 +1,21 @@
 { config, pkgs, lib, ... }:
 
 {
-  # ... (other settings)
+  fonts.packages = [
+  ] ++ builtins.filter lib.attrsets.isDerivation (builtins.attrValues pkgs.nerd-fonts);
+
+  xdg.portal.wlr.enable = true;
+  xdg.portal.enable = true;
+  xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
 
   environment.sessionVariables = {
     OPENSSL_DIR = "${pkgs.openssl.dev}";
     OPENSSL_LIB_DIR = "${pkgs.openssl.out}/lib";
     OPENSSL_INCLUDE_DIR = "${pkgs.openssl.dev}/include";
-    PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
   };
-
   environment.extraInit = ''
-    export PKG_CONFIG_PATH=${pkgs.openssl.dev}/lib/pkgconfig:$PKG_CONFIG_PATH
+    # This prepends the OpenSSL path to the existing path, 
+    # ensuring the default GLib/GTK paths are preserved.
+    export PKG_CONFIG_PATH="${pkgs.openssl.dev}/lib/pkgconfig:$PKG_CONFIG_PATH"
   '';
-
 }
