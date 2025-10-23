1e240b5d938b3fbf009b3e8bbfd291384e0d6b09
doyouwantto2 <tinhphong2580@gmail.com>
2025-10-24 01:59:41 +0900

    change pkg-config path

--- nixos/system/startup/native.nix
+++ nixos/system/startup/native.nix
@@ -1,18 +1,16 @@
 { config, pkgs, lib, ... }:
 
 {
-  fonts.packages = [
-  ] ++ builtins.filter lib.attrsets.isDerivation (builtins.attrValues pkgs.nerd-fonts);
-
-  xdg.portal.wlr.enable = true;
-  xdg.portal.enable = true;
-  xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+  # ... (other settings)
 
   environment.sessionVariables = {
     OPENSSL_DIR = "${pkgs.openssl.dev}";
     OPENSSL_LIB_DIR = "${pkgs.openssl.out}/lib";
     OPENSSL_INCLUDE_DIR = "${pkgs.openssl.dev}/include";
-    PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
   };
+
+  environment.extraInit = ''
+    export PKG_CONFIG_PATH=${pkgs.openssl.dev}/lib/pkgconfig:$PKG_CONFIG_PATH
+  '';
 
 }
