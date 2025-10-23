13800b5548c65edbd35d91f9f9e7e947337c75d1
doyouwantto2 <tinhphong2580@gmail.com>
2025-10-24 02:13:58 +0900

    update

--- nixos/system/startup/native.nix
+++ nixos/system/startup/native.nix
@@ -13,9 +13,4 @@
     OPENSSL_LIB_DIR = "${pkgs.openssl.out}/lib";
     OPENSSL_INCLUDE_DIR = "${pkgs.openssl.dev}/include";
   };
-  environment.extraInit = ''
-    # This prepends the OpenSSL path to the existing path, 
-    # ensuring the default GLib/GTK paths are preserved.
-    export PKG_CONFIG_PATH="${pkgs.openssl.dev}/lib/pkgconfig:$PKG_CONFIG_PATH"
-  '';
 }
