3618845b62842e7ab250d6a8d269358f45e376ba
doyouwantto2 <tinhphong2580@gmail.com>
2025-10-27 02:00:37 +0900

    format

--- nixos/user/file.nix
+++ nixos/user/file.nix
@@ -2,11 +2,9 @@
 
 {
   home.file = {
-    "${config.xdg.configHome}/hypr" = {
-      source = config.lib.file.mkOutOfStoreSymlink ./extra/dotfiles/hypr;
-    };
-
     "${config.xdg.configHome}/nvim".source = config.lib.file.mkOutOfStoreSymlink ./extra/dotfiles/nvim;
     "${config.xdg.configHome}/cava".source = config.lib.file.mkOutOfStoreSymlink ./extra/dotfiles/cava;
+
+    "${config.xdg.configHome}/hypr".source = config.lib.file.mkOutOfStoreSymlink ./extra/dotfiles/hypr;
   };
 }
