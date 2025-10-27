d1845ab6cc89970ea344980506f1bd706b8a543b
doyouwantto2 <tinhphong2580@gmail.com>
2025-10-27 23:19:45 +0900

    add wezterm to home

--- nixos/user/file.nix
+++ nixos/user/file.nix
@@ -4,6 +4,7 @@
   home.file = {
     "${config.xdg.configHome}/nvim".source = config.lib.file.mkOutOfStoreSymlink ./extra/dotfiles/nvim;
     "${config.xdg.configHome}/cava".source = config.lib.file.mkOutOfStoreSymlink ./extra/dotfiles/cava;
+    "${config.xdg.configHome}/wezterm".source = config.lib.file.mkOutOfStoreSymlink ./extra/dotfiles/wezterm;
 
     "${config.xdg.configHome}/hypr".source = config.lib.file.mkOutOfStoreSymlink ./extra/dotfiles/hypr;
   };
